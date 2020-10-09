53a5c095f76e68b893d38d30c04501365d958c0d
nahuelcosta <nahueecosta@gmail.com>
2020-10-09 15:15:50 -0300

    update

---
+++ Assets/RandomAudioClip.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAudioClip: MonoBehaviour {
+    
+    public AudioClip[] clips;
+    public AudioSource audioSource;
+
+    void Start (){
+        audioSource = FindObjectOfType<AudioSource>();
+        audioSource.loop = false;
+    }
+    public AudioClip GetRandomClip()
+    {
+        return clips[Random.Range(0,clips.Length)];
+    }
+    void Update (){
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip=GetRandomClip();
+            audioSource.Play();
+        }
+    }
+}
